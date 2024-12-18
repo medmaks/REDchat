c2f3eec57db1a785a7f95419369cdf7fbd0bdc15
medmaks <yltramaks731@gmail.com>
2024-12-19 00:13:57 +0200

    1

--- ws/client.js
+++ ws/client.js
@@ -1,21 +1,56 @@
-const {WebSocket} = require('ws');
+const { WebSocket } = require('ws');
+const readline = require('node:readline');
+const { ChatClient } = require('./client/ChatClient');
 
-const wsClientFactory = (id) => {
-    const ws = new WebSocket('ws://localhost:8080');
+const createWebSocketClient = (clientId) => {
+    const socket = new WebSocket('ws://localhost:8080');
 
-    ws.on('error', console.error);
+    socket.on('error', (err) => console.error(err));
 
-    ws.on('open', function open() {
-        console.log('connected ' + (id) + ' ')
-
-        ws.send(' ' + (id) + ' Hello');
+    socket.on('open', () => {
+        console.log(`Client ${clientId} connected`);
+        socket.send(`${clientId} says HELLO`);
     });
 
-    ws.on('message', function message(data) {
-      console.log(' ' + (id) + ' received: %s', data);
+    socket.on('message', (message) => {
+        console.log(`Client ${clientId} received: ${message}`);
+    });
+
+    return socket;
+};
+
+const sessionIdArgIndex = process.argv.indexOf('--sessionId');
+const nameArgIndex = process.argv.indexOf('--name');
+
+if (sessionIdArgIndex === -1 && nameArgIndex === -1) {
+    console.error('Error: --sessionId or --name arguments are required');
+    process.exit(1);
+}
+
+const sessionId = sessionIdArgIndex !== -1 ? process.argv[sessionIdArgIndex + 1] : null;
+const username = nameArgIndex !== -1 ? process.argv[nameArgIndex + 1] : null;
+
+initializeChat(username, sessionId);
+
+function initializeChat(username, sessionId) {
+    const chatClient = new ChatClient({ url: 'ws://localhost:8080', username, sessionId });
+
+    chatClient.init();
+
+    const chatInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    chatInterface.on('line', (input) => {
+        if (input.trim().toLowerCase() === 'exit') {
+            chatInterface.close();
+        } else {
+            chatClient.send(input);
+        }
     });
 }
 
-const wsClient1 = wsClientFactory(1);
-const wsClient2 = wsClientFactory(2);
-const wsClient3 = wsClientFactory(3);
+const client1 = createWebSocketClient(1);
+const client2 = createWebSocketClient(2);
+const client3 = createWebSocketClient(3);
